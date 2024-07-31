6e6edaf824e9bf0e52b11d0b9c381745202e6786
Vasant Singh <84444859+vasantsingh@users.noreply.github.com>
2024-07-31 14:29:33 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './src/tests',
-    timeout: 70000,
+    timeout: 1000000,
     use: {
         browserName: 'chromium',
         headless: true,
         baseURL: 'https://www.zoho.com/en-in/crm/',
         trace: 'on',
-        actionTimeout: 10000, 
+        actionTimeout: 70000, 
         screenshot: 'only-on-failure',
-        navigationTimeout: 30000,
+        navigationTimeout: 80000,
     },
     fullyParallel:false,
     reporter:[
